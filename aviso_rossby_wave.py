d504da43a905556e23793b87b93ebc91ecab777f
Yu Gao <36052692+yugaophd@users.noreply.github.com>
2023-08-04 15:12:01 -0700

    finalize figures

--- aviso_rossby_wave.py
+++ aviso_rossby_wave.py
@@ -54,38 +54,38 @@ def skill_matrix(MSLA, Psi, k_n, l_n, MModes, Rm, lon, lat, T_time):
             for mm in range(MModes):
                 omega[ll, kk, mm] =  Beta * k_n[kk, mm] / (l_n[ll, mm] ** 2 + k_n[kk, mm] ** 2 + Rm[mm] ** -2) # non-dispersive wave
 
-    with tqdm(total= len(l_n) * len(k_n)* MModes) as pbar:
-        for nn in range(len(l_n)):
-            for ll in range(len(k_n)):
-                for mm in range(MModes):
-                    for count in range(len(Iindex)):
-                        # change lon, lat to (dlon, dlat = (lon, lat) - mean
-                        # conversion to distance 
-                        H0[count, 0] = Psi[0, mm] * np.cos(l_n[nn, mm] * dlon[int(Iindex[count])] + k_n[ll, mm] * dlat[int(Jindex[count])] + omega[nn, ll, mm] * T_time[int(Tindex[count])]) 
-                        H0[count, 1] = Psi[0, mm] * np.sin(l_n[nn, mm] * dlon[int(Iindex[count])] + k_n[ll, mm] * dlat[int(Jindex[count])] + omega[nn, ll, mm] * T_time[int(Tindex[count])])       
+#with tqdm(total= len(l_n) * len(k_n)* MModes) as pbar:
+    for nn in range(len(l_n)):
+        for ll in range(len(k_n)):
+            for mm in range(MModes):
+                for count in range(len(Iindex)):
+                    # change lon, lat to (dlon, dlat = (lon, lat) - mean
+                    # conversion to distance 
+                    H0[count, 0] = Psi[0, mm] * np.cos(l_n[nn, mm] * dlon[int(Iindex[count])] + k_n[ll, mm] * dlat[int(Jindex[count])] + omega[nn, ll, mm] * T_time[int(Tindex[count])]) 
+                    H0[count, 1] = Psi[0, mm] * np.sin(l_n[nn, mm] * dlon[int(Iindex[count])] + k_n[ll, mm] * dlat[int(Jindex[count])] + omega[nn, ll, mm] * T_time[int(Tindex[count])])       
 
-                    M = 2
+                M = 2
 
-                    RR, PP = .1, 1
+                RR, PP = .1, 1
 
-                    HTH = np.matmul(H0.T, H0)
+                HTH = np.matmul(H0.T, H0)
 
-                    for pp in range(M):
-                        HTH[pp, pp] = HTH[pp, pp] +  RR/PP
+                for pp in range(M):
+                    HTH[pp, pp] = HTH[pp, pp] +  RR/PP
 
-                    D = np.matmul(LA.inv(HTH), H0.T)   
+                D = np.matmul(LA.inv(HTH), H0.T)  
 
-                    X_ = np.matmul(D, SSHA_vector)
+                X_ = np.matmul(D, SSHA_vector)
 
-                    # calculate residual
-                    residual = SSHA_vector - np.matmul(H0, X_)
+                # calculate residual
+                residual = SSHA_vector - np.matmul(H0, X_)
 
-                    # variance of residual
-                    # evaluate skill (1- rms_residual/rms_ssha_vector) and store the skill
-                    # skill value nn, ll, mm, = skill value
-                    skill[nn, ll, mm] = 1 - np.sqrt(np.mean(residual**2)) / np.sqrt(np.mean(SSHA_vector**2))
+                # variance of residual
+                # evaluate skill (1- rms_residual/rms_ssha_vector) and store the skill
+                # skill value nn, ll, mm, = skill value
+                skill[nn, ll, mm] = 1 - np.sqrt(np.mean(residual**2)) / np.sqrt(np.mean(SSHA_vector**2))
 
-                    pbar.update(1)
+                #pbar.update(1)
                     
     return skill, SSHA_vector, Iindex, Jindex, Tindex
 
@@ -103,11 +103,17 @@ def inversion(Y, H_v, P_over_R):
 
     HTH = np.matmul(H_v.T, H_v)
     
-    HTH = HTH +  P_over_R #, P: uncertainty in model, R: uncertainty in data
+    #P_diag = P_over_R.diagonal()
+    
+    #P_over_R_inv = np.zeros(P_over_R.shape)
+    
+    #np.fill_diagonal(P_over_R_inv, P_diag ** -1)
+    
+    HTH = HTH +  P_over_R #, P: uncertainty in model, R: uncertainty in data, actually R_over_P
     
     D = np.matmul(LA.inv(HTH), H_v.T)
     
-    eig, vec = LA.eig(HTH)
+    #eig, vec = LA.eig(HTH)
     
     amp = np.matmul(D, Y)
     
@@ -115,6 +121,40 @@ def inversion(Y, H_v, P_over_R):
     
     return amp, Y_estimated
 
+def inversion2(Y, H_v, P_over_R):
+    
+    '''
+    Solve for X given observations (Y), basis function (H_v) and signal to noise ratio (P_over_R).
+    Return: X (amplitudes of Rossby waves)
+    This is all in model space.
+    '''
+    
+    import numpy as np
+    from numpy import linalg as LA
+
+    #R_factor = 0.01 ** 2 # 1 cm noise
+    
+    R_factor = 0.001 ** 2 # .1 cm noise
+    
+    R_factor_inv = 1 / R_factor
+    
+    HTH = np.matmul(H_v.T, H_v) * R_factor_inv 
+    
+    P_diag = P_over_R.diagonal()
+    
+    P_over_R_inv = np.zeros(P_over_R.shape)
+    
+    np.fill_diagonal(P_over_R_inv, P_diag ** -1)
+    
+    HTH = HTH +  P_over_R_inv #, P: uncertainty in model, R: uncertainty in data, actually R_over_P
+    
+    D = np.matmul(LA.inv(HTH), H_v.T * R_factor_inv) 
+    
+    amp = np.matmul(D, Y)
+    
+    Y_estimated = np.matmul(H_v, amp)
+    
+    return amp, Y_estimated
 
 def forecast_ssh(MSLA, amp, H_all):
     
@@ -287,9 +327,6 @@ def build_swath(swath_width, x_width, day, lon, lat):
     xswath_index_right = np.ma.MaskedArray(xswath_index_right, y_mask_right)
     yswath_index_right = np.ma.MaskedArray(yswath_index_right, x_mask_right)
 
-    #yindex = np.tile(np.append(yswath_index_left.compressed().astype(int), yswath_index_right.compressed().astype(int)), len(day))
-    #xindex = np.tile(np.append(xswath_index_left.compressed().astype(int), xswath_index_right.compressed().astype(int)), len(day))    
-    #tindex = np.repeat(day, len(xindex)//len(day))
     yvalid_index = np.append(yswath_index_left.compressed().astype(int), yswath_index_right.compressed().astype(int)) 
     xvalid_index = np.append(xswath_index_left.compressed().astype(int), xswath_index_right.compressed().astype(int))
     
@@ -334,7 +371,6 @@ The output of the function includes the valid data points of the timing error, r
     H_neg_left, H_pos_left = np.ma.masked_all(yswath_index_left.shape), np.ma.masked_all(yswath_index_left.shape)
 
     for xx in np.arange(ac):
-        #print(xx)
         xc1_left[:, xx] = (xx - xc)  #* .25     #.25 degree resolution
         xc2_left[:, xx] = (xx - xc)  ** 2  #* .25
         # timing error = alpha[0] * X^0 
@@ -434,7 +470,6 @@ The output of the function includes the valid data points of the timing error, r
     import numpy as np
     
     Tdim, ALdim, ACdim = len(days), yswath_index_left.shape[0], yswath_index_left.shape[1] # time dimension, along-track and across-track dimesion respectively
-    print(Tdim, ALdim, ACdim)
     # timing error
     timing_err_left, timing_err_right = np.ma.masked_all([Tdim, ALdim, ACdim]), np.ma.masked_all([Tdim, ALdim, ACdim])
     # Roll error
@@ -456,7 +491,6 @@ The output of the function includes the valid data points of the timing error, r
 
     for xx in np.arange(ACdim):
         for tt in np.arange(Tdim):
-            #print(xx)
             xc1_left[:, xx] = (xx - xc)  #* .25     #.25 degree resolution
             xc2_left[:, xx] = (xx - xc)  ** 2  #* .25
             # timing error = alpha[0] * X^0 
@@ -536,7 +570,6 @@ The output of the function includes the valid data points of the timing error, r
         
         if tt == 0: 
             valid_points = len(timing_err_left_masked.compressed()) + len(timing_err_right_masked.compressed())
-            print(valid_points)
             timing_err_valid = np.ma.masked_all([Tdim, valid_points]) # dimensions: time, valid data points
             roll_err_valid = np.ma.masked_all([Tdim, valid_points]) # dimensions: time, valid data points
             baseline_dilation_err_valid = np.ma.masked_all([Tdim, valid_points]) # dimensions: time, valid data points
@@ -556,3 +589,158 @@ The output of the function includes the valid data points of the timing error, r
     return timing_err_valid, roll_err_valid, baseline_dilation_err_valid, phase_err_valid, xc1_valid, xc2_valid 
 
 
+def calculate_errors(Tdim, Valid_points, ssh_estimated_swath, ssh, cor_err, err_estimated_swath, MSLA_swath, xvalid_index, yvalid_index, lon, lat, date_time):
+    import matplotlib.pyplot as plt
+    import cmocean as cmo
+    import numpy as np
+    
+    ssh_diff = ssh_estimated_swath - ssh
+    err_diff = np.sqrt(np.mean((cor_err - err_estimated_swath)**2, axis=1)) / np.sqrt(np.mean(cor_err**2, axis=1))
+    ssh_diff_percent = np.sqrt(np.mean(ssh_diff**2, axis=1)) / np.sqrt(np.mean(ssh**2, axis=1))
+    err_map = np.zeros([Tdim, len(lon), len(lat)])
+    ssh_map = np.zeros([Tdim, len(lon), len(lat)])
+    ssh_true = np.zeros([Tdim, len(lon), len(lat)])
+    err_true = np.zeros([Tdim, len(lon), len(lat)])
+    msla_obs = np.zeros([Tdim, len(lon), len(lat)])
+
+    for tt in range(Tdim):
+        for ii in range(Valid_points):
+            err_map[tt, xvalid_index[ii], yvalid_index[ii]] = err_estimated_swath[tt, ii]
+            ssh_map[tt, xvalid_index[ii], yvalid_index[ii]] = ssh_estimated_swath[tt, ii]
+            err_true[tt, xvalid_index[ii], yvalid_index[ii]] = cor_err[tt, ii]
+            ssh_true[tt, xvalid_index[ii], yvalid_index[ii]] = ssh[tt, ii]
+            msla_obs[tt, xvalid_index[ii], yvalid_index[ii]] = MSLA_swath[tt, ii]
+
+        err_diff1 = np.sqrt(np.mean((err_true[tt] - err_map[tt])**2)) / np.sqrt(np.mean(err_true[tt]**2))
+        ssh_diff1 = np.sqrt(np.mean((ssh_true[tt] - ssh_map[tt])**2)) / np.sqrt(np.mean(ssh_true[tt]**2))
+
+    return err_diff, ssh_diff_percent, err_map, ssh_map, err_true, ssh_true
+
+
+def plot_time_series(tt, ssh, MSLA_swath, ssh_estimated_swath, ssh_diff, ssh_diff_percent, cor_err, err_estimated_swath, err_diff):
+    
+    import matplotlib.pyplot as plt
+    import cmocean as cmo
+
+    plt.figure(figsize=(10, 8))
+
+    plt.subplot(311)
+    plt.plot(ssh[tt], 'b-', label='True SSH')
+    plt.plot(MSLA_swath[tt], 'g', label = 'True SSH + Error')
+    plt.plot(ssh_estimated_swath[tt], 'r--', label='Estimated SSH')
+    plt.xlabel('Data point', fontsize=14)
+    plt.ylabel('SSH (m)', fontsize=14)
+    plt.title('SSH estimation', fontsize=16)
+    plt.legend(fontsize=12)
+
+    plt.subplot(312)
+    plt.plot(ssh_diff[tt], 'b', label='True SSH - SSH estimate')
+    plt.xlabel('Data point', fontsize=14)
+    plt.ylabel('SSH (m)', fontsize=14)
+    plt.title('True SSH - SSH estimate, ' + str(ssh_diff_percent[tt] * 100)[:5] + '%', fontsize=16)
+    plt.legend(fontsize=12)
+    plt.tight_layout()
+
+    plt.subplot(313)
+    plt.plot(cor_err[tt], 'b-', label='True error')
+    plt.plot(err_estimated_swath[tt], 'r--', label='Estimated error')
+    plt.legend(fontsize=12)
+    plt.title('True error - error estimate, ' + str(err_diff[tt] * 100)[:5] + '%', fontsize=16)
+
+def plot_ssh_err_estimate(tt, date_time, ssh, MSLA_swath, ssh_estimated_swath, ssh_diff, ssh_diff_percent, cor_err, err_estimated_swath, err_diff):
+    
+    import matplotlib.pyplot as plt
+    import cmocean as cmo
+
+    plot_time_series(tt, ssh, MSLA_swath, ssh_estimated_swath, ssh_diff, ssh_diff_percent, cor_err, err_estimated_swath, err_diff)
+    plt.savefig('./ssh_err_estimate/ssh_err_parameter_'+  str(date_time[tt])[:10] +'.png')
+    #plt.close()
+
+def plot_ssh_err_est_maps(tt, day0, day1, lon, lat, err_true, ssh_true, ssh_map, err_map, err_diff, ssh_diff):
+    
+    import matplotlib.pyplot as plt
+    import cmocean as cmo
+    
+    fig = plt.figure(figsize = (10, 12))
+
+    plt.subplot(321)
+    plt.pcolormesh(lon, lat, err_true+ ssh_true, cmap = cmo.cm.balance, vmin = -.2, vmax = .2)
+    plt.colorbar()
+    plt.xlabel('Longitude (\N{DEGREE SIGN}W)', fontsize = 14)
+    plt.ylabel('Latitude (\N{DEGREE SIGN}N)', fontsize = 14)
+    plt.title('a) True error + true SSH', fontsize = 14)
+
+    plt.subplot(323)
+    plt.pcolormesh(lon, lat, ssh_map, vmin = -.2, vmax = .2, cmap = cmo.cm.balance)
+    plt.colorbar()
+    plt.xlabel('Longitude (\N{DEGREE SIGN}W)', fontsize = 14)
+    plt.ylabel('Latitude (\N{DEGREE SIGN}N)', fontsize = 14)
+    plt.title('c) SSH estimate, 1-stage ' + str((1- ssh_diff[tt]) * 100)[:5] + '%', fontsize = 14)
+
+    plt.subplot(325)
+    plt.pcolormesh(lon, lat, ssh_true - ssh_map, cmap = cmo.cm.balance, vmin = -.2, vmax = .2)
+    plt.colorbar()
+    plt.xlabel('Longitude (\N{DEGREE SIGN}W)', fontsize = 14)
+    plt.ylabel('Latitude (\N{DEGREE SIGN}N)', fontsize = 14)
+    plt.title('e) True SSH - SSH estimate, ' + str(ssh_diff[tt] * 100)[:5] + '%', fontsize = 14)
+
+    plt.subplot(322)
+    plt.pcolormesh(lon, lat, err_true, cmap = cmo.cm.balance, vmin = -.2, vmax = .2)
+    plt.colorbar()
+    plt.xlabel('Longitude (\N{DEGREE SIGN}W)', fontsize = 14)
+    plt.ylabel('Latitude (\N{DEGREE SIGN}N)', fontsize = 14)
+    plt.title('b) True error', fontsize = 14)
+
+    plt.subplot(324)
+    plt.pcolormesh(lon, lat, err_map, cmap = cmo.cm.balance, vmin = -.2, vmax = .2)
+    plt.colorbar()
+    plt.xlabel('Longitude (\N{DEGREE SIGN}W)', fontsize = 14)
+    plt.ylabel('Latitude (\N{DEGREE SIGN}N)', fontsize = 14)
+    plt.title('d) Error estimate ' +  str((1- err_diff[tt]) * 100)[:4] + '%', fontsize = 14)
+
+    plt.subplot(326)
+    plt.pcolormesh(lon, lat, err_true - err_map, cmap = cmo.cm.balance, vmin = -.2, vmax = .2)
+    plt.colorbar()
+    plt.xlabel('Longitude (\N{DEGREE SIGN}W)', fontsize = 14)
+    plt.ylabel('Latitude (\N{DEGREE SIGN}N)', fontsize = 14)
+    plt.title('f) True Error - Error estimate, ' + str(err_diff[tt] * 100)[:4] + '%' , fontsize = 14)
+
+    plt.tight_layout()
+    plt.savefig('ssh_err_est_maps/aviso_ssh_estimate_'+ str(date_time[tt])[:10] +
+                '_1step_' + str(int(day1 - day0)*5) +'day.png', dpi = 300)
+    #plt.close()
+
+def calculate_ssh_err_diff(Tdim, Valid_points, ssh_estimated_swath, ssh, err_est_1step, cor_err):
+
+    import numpy as np
+
+    ssh_diff = np.zeros([Tdim, Valid_points])
+    err_diff = np.zeros([Tdim, Valid_points])
+    ssh_diff_percent, err_diff_percent = np.zeros([Tdim]), np.zeros([Tdim])
+
+    for tt in range(Tdim):
+        ssh_diff[tt] = ssh_estimated_swath[tt * Valid_points : (tt+1) * Valid_points] - ssh[tt * Valid_points : (tt+1) * Valid_points]
+        err_diff[tt] = err_est_1step[tt * Valid_points : (tt+1) * Valid_points] - cor_err[tt * Valid_points : (tt+1) * Valid_points]
+        ssh_diff_percent[tt] = np.sqrt(ssh_diff[tt]**2).mean() / np.sqrt(ssh[tt * Valid_points : (tt+1) * Valid_points]**2).mean()
+        err_diff_percent[tt]  = np.sqrt(np.mean(err_diff[tt]**2).mean()) / np.sqrt(cor_err[tt * Valid_points : (tt+1) * Valid_points]**2).mean()
+
+    return ssh_diff, err_diff, ssh_diff_percent, err_diff_percent
+
+def remap_to_2d_map(tt, Valid_points, lon, lat, ssh_estimated_swath, ssh,  err_est_1step, cor_err, xvalid_index, yvalid_index):
+
+    import numpy as np
+    
+    err_map = np.zeros([len(lon), len(lat)])
+    ssh_map = np.zeros([len(lon), len(lat)])
+    ssh_true = np.zeros([len(lon), len(lat)])
+    err_true = np.zeros([len(lon), len(lat)])
+    #msla_obs = np.zeros([len(lon), len(lat)])
+
+    for ii in range(Valid_points):
+        err_map[xvalid_index[ii], yvalid_index[ii]] = err_est_1step[tt * Valid_points : (tt+1) * Valid_points][ii]
+        ssh_map[xvalid_index[ii], yvalid_index[ii]]  = ssh_estimated_swath[tt * Valid_points : (tt+1) * Valid_points][ii]
+        err_true[xvalid_index[ii], yvalid_index[ii]] = cor_err[tt * Valid_points : (tt+1) * Valid_points][ii]
+        ssh_true[xvalid_index[ii], yvalid_index[ii]] = ssh[tt * Valid_points : (tt+1) * Valid_points][ii]
+        #msla_obs[xvalid_index[ii], yvalid_index[ii]] = MSLA_swath[tt * Valid_points : (tt+1) * Valid_points][ii]
+
+    return ssh_map, ssh_true, err_map, err_true#, msla_obs
